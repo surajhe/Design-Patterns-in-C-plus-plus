fc849fcc739248c17d786e0b567437666d447a40
surajhe <86972874+surajhe@users.noreply.github.com>
2022-11-29 23:19:19 +0530

    Create ThreadSafeSingleton.cpp

---
+++ ThreadSafeSingleton.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <mutex>
+using namespace std;
+
+class Singleton
+{
+	private : 
+			 
+		Singleton();
+		static Singleton* obj;
+		
+	public :
+		
+		static Singleton GetInstance()
+		{
+			if(!obj)
+			{
+				mutex _mutex;
+				lock_guard<mutex> _lock(_mutex);
+				if(!obj)
+				{
+					obj = new Singleton();	
+				}
+			}
+			return *obj;
+		}
+};
+
+int main() {
+	// your code goes here
+	return 0;
+}

a241646bac9b998a648716bb061bf834b24a9794
surajhe <86972874+surajhe@users.noreply.github.com>
2023-03-01 18:26:52 +0530

    Create ObserverDesignPattern.cpp

---
+++ ObserverDesignPattern.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Observer design pattern
+// it is used to notify objects if some changes happens in another objects.
+// It is also known as publish-subscribe pattern.
+// In this pattern observers need to subscribed/registered to subject and if some changes 
+// happens in subject then it notifies to observers.
+// Example - one to many dependency between objects.
+// suppose i am writting file monitoring application where i need to notify Listeners when 
+// some file changes happens in a directory.
+// File changes can be Adding a file, removing a file, updating a file, accessing a file.
+
+
+
+int main() {
+	// your code goes here
+	return 0;
+}
